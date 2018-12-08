35294cc68650ba32d0d4dd514892a7063c8835d0
Josh Salyer <joshsalyer23@gmail.com>
2018-12-07 18:27:43 -0600

    Adds base GET data functions to DataService

--- contact-manager-ng6/src/app/data.service.ts
+++ contact-manager-ng6/src/app/data.service.ts
@@ -11,4 +11,14 @@ export class DataService {
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
+
+  getUser(userId: number) {
+
+
+    return this.http.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+  }
+
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
 }
