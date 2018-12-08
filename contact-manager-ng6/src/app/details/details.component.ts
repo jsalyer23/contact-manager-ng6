91cf980c6a0a492d41a173a8cccf6ebf11a1f120
Josh Salyer <joshsalyer23@gmail.com>
2018-12-07 18:26:45 -0600

    Adds DataService and subscription for posts, user details, and user components

--- contact-manager-ng6/src/app/details/details.component.ts
+++ contact-manager-ng6/src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private user: Object;
+  private userId: number;
+
+  constructor(private data: DataService, private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => { this.userId = params.id; });
+  }
 
   ngOnInit() {
+    this.data.getUser(this.userId).subscribe((data) => { this.user = data; });
   }
 
 }
