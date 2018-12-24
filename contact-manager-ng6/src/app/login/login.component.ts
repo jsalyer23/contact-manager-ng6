aaae36dd2774d0342cb5f9fd6219327b5f67460e
Josh Salyer <joshsalyer23@gmail.com>
2018-12-23 21:56:38 -0600

    Added base style for LoginComponent, builds user login actions and form control

--- contact-manager-ng6/src/app/login/login.component.ts
+++ contact-manager-ng6/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularTokenService } from 'angular-token';
+import { MatCardModule } from '@angular/material';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  currentUser: Object;
+  public email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  public password: FormControl = new FormControl('', [Validators.required]);
+  public passwordConfirmation: FormControl = new FormControl('', [Validators.required])
 
-  ngOnInit() {
+  constructor(private tokenService: AngularTokenService) {}
+
+  ngOnInit() {}
+
+  private loginUser() {
+    // If there is no email or password provided, don't make the request
+    if (!this.email.value || !this.password.value) { return; }
+    this.tokenService.signIn({ login: this.email.value, password: this.password.value })
+      .subscribe(
+        (response) => {
+          // handle response and navigate to their profile
+          this.currentUser = response.body.data;
+          console.log(response),
+        (error) => {
+          // handle error and encourage them to try again
+          console.error(error); } });
   }
 
+  private registerUser() {
+    this.tokenService.registerAccount({
+      login: this.email.value, password: this.password.value, passwordConfirmation: this.passwordConfirmation.value
+    }).subscribe((response) => { console.log(response), (error) => { console.error(error); } })
+  }
 }
