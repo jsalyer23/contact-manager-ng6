f9b131f4392f9891407d1753e54c4d6205d26834
Josh Salyer <joshsalyer23@gmail.com>
2018-12-23 19:33:50 -0600

    Adds AngularTokenService to AppComponent along with base login and registration functions

--- contact-manager-ng6/src/app/app.component.ts
+++ contact-manager-ng6/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularTokenService } from 'angular-token';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contact-manager-ng6';
+
+  constructor(private tokenService: AngularTokenService) {
+    // this.loginUser('user@example.com', 'monkey67');
+  }
+
+  private loginUser(login: string, password: string) {
+    this.tokenService.signIn({login: login, password: password})
+        .subscribe((response) => { console.log(response), (error) => { console.error(error);}});
+  }
+
+  private registerUser(login: string, password: string, passwordConfirmation: string) {
+    this.tokenService.registerAccount({
+      login: login, password: password, passwordConfirmation: passwordConfirmation
+    }).subscribe((response) => { console.log(response), (error) => { console.error(error); }})
+  }
 }
