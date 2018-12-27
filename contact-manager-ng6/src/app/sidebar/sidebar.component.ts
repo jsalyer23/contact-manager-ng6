f5b2480c5b078224c7b2cdf55546936f7aeb95a7
Josh Salyer <joshsalyer23@gmail.com>
2018-12-26 19:44:20 -0600

    Updates SideBarComponent to implement login and profile links, adds default navigation to home view after user is signed out

--- contact-manager-ng6/src/app/sidebar/sidebar.component.ts
+++ contact-manager-ng6/src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,13 +11,20 @@ export class SidebarComponent implements OnInit {
 
   private currentUrl: String;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public authService: AuthService) {
     router.events.subscribe((_: NavigationEnd) => { this.currentUrl = router.url });
     console.log(this.currentUrl);
   }
 
   ngOnInit() {
     console.log(this.currentUrl);
+  }
+
+  /**
+   * Logs out current user and navigates back to Home view
+   */
+  public logOutUser() {
+    this.authService.logOutUser().subscribe(() => { this.router.navigate(['/'])});
   }
 
 }
